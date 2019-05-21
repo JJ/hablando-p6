2506cccf23d7a886b9c538fbb639f47912928113
JJ Merelo <jjmerelo@gmail.com>
2019-05-21 21:08:28 +0200

    Hasta gradual typing

--- code/lambda.py
+++ code/lambda.py
@@ -3,6 +3,8 @@
 from functools import reduce;
 
 def fib(n):
-    return reduce( lambda prev,this: \
-	           prev+[prev[-2]+prev[-1]], range(1,n+1), [1,1] )
+    return reduce( lambda prev,this: prev+[ prev[-2]+prev[-1] ], \
+                   range(1,n+1), \
+                   [1,1] )
+
 print(fib(12))
