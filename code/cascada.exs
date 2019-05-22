ba064bc5426842b380ff036c25cb912edb3875f5
JJ Merelo <jjmerelo@gmail.com>
2019-05-22 19:42:36 +0200

    Cambiamos de tercio

---
+++ code/cascada.exs
@@ -0,0 +1,7 @@
+resultado = 1..100
+      |> Enum.filter(fn a -> rem(a,2) == 0 end)
+      |> Enum.map(fn a -> :math.pow(a,2) end)
+      |> Enum.reduce( &(&1 + &2) )
+
+IO.puts  resultado
+
--- code/collatz.exs
+++
@@ -1,7 +0,0 @@
-
-resultado = [1,2,3,4,5,6,7,8,9,10]
-|> Enum.map(fn a -> a*2 end)
-|> Enum.filter(fn a -> rem(a, 3) == 0 end)
-|> Enum.reduce(fn a,b -> a + b end)
-IO.puts  resultado
-
