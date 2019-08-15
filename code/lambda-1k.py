535fb728165312bacfc21a034525a831b53df22a
JJ Merelo <jjmerelo@gmail.com>
2019-08-15 17:31:40 +0200

    Benchmarking lambdas

---
+++ code/lambda-1k.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from functools import reduce;
+
+def fib(n):
+    return reduce( lambda prev,this: prev+[ prev[-2]+prev[-1] ], \
+                   range(1,n+1), \
+                   [1,1] )
+
+print(fib(10000)[10000])
